a478de7948eb93a47d38c685f8169e5a77c4d8e3
Maoz Tamir <maoztamir@h-MBP-sl-Maoz.lan>
2018-06-09 15:44:36 +0300

    added child routes, all task functionality and localstorage support

--- public/assets/js/controllers/todos_controller.js
+++ public/assets/js/controllers/todos_controller.js
@@ -1,5 +1,11 @@
 Todos.TodosController = Ember.ArrayController.extend({
     actions: {
+        clearCompleted: function() {
+            var completed = this.filterBy('isCompleted', true);
+            // invoke - execute those methods on each object in the Array if the method exists on that object
+            completed.invoke('deleteRecord');
+            completed.invoke('save');
+        },
         createTodo: function() {
             // Get the todo title set by the "New Todo" text field
             var title = this.get('newTitle');
@@ -19,10 +25,38 @@ Todos.TodosController = Ember.ArrayController.extend({
         }
     },
 
+
+    /** check if all the todos are done */
+    allAreDone: function(key, value) {
+        if (value === undefined) {
+            return !!this.get('length') && this.isEvery('isCompleted', true);
+        } else {
+            this.setEach('isCompleted', value);
+            this.invoke('save');
+            return value;
+        }
+    }.property('@each.isCompleted'),
+
+
+    /** check if any completed exist */
+    hasCompleted: function() {
+        return this.get('completed') > 0;
+    }.property('completed'),
+
+
+    /**
+     * returns an instance of EmberArray which contains only the items for which the callback returns true.
+     */
+    completed: function() {
+        return this.filterBy('isCompleted', true).get('length');
+    }.property('@each.isCompleted'),
+
+
     /** number of todos whose isCompleted */
     remaining: function() {
         return this.filterBy('isCompleted', false).get('length');
     }.property('@each.isCompleted'),
+
 
     /** either a plural or singular version of the word "item"  */
     inflection: function() {
--- public/assets/js/router.js
+++ public/assets/js/router.js
@@ -1,9 +1,44 @@
 Todos.Router.map(function() {
-    this.resource('todos', { path: '/' });
+    this.resource('todos', { path: '/' }, function() {
+        // additional child routes
+        this.route('active');
+        this.route('completed');
+    });
 });
 
 Todos.TodosRoute = Ember.Route.extend({
     model: function() {
         return this.store.find('todo');
+    }
+});
+
+/**  index route */
+Todos.TodosIndexRoute = Ember.Route.extend({
+    model: function() {
+        return this.modelFor('todos');
+    }
+});
+
+/** route for active todos */
+Todos.TodosActiveRoute = Ember.Route.extend({
+    model: function(){
+        return this.store.filter('todo', function(todo) {
+            return !todo.get('isCompleted');
+        });
+    },
+    renderTemplate: function(controller) {
+        this.render('todos/index', {controller: controller});
+    }
+});
+
+/** route for completed todos */
+Todos.TodosCompletedRoute = Ember.Route.extend({
+    model: function() {
+        return this.store.filter('todo', function(todo) {
+            return todo.get('isCompleted');
+        });
+    },
+    renderTemplate: function(controller) {
+        this.render('todos/index', {controller: controller});
     }
 });
